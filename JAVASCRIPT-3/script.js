3e555dd0207806ae24d4758e40014a321180e176
Aditya Goswami <aadigithub61@gmail.com>
2025-08-15 01:25:27 +0530

    Add files via upload

---
+++ JAVASCRIPT-3/script.js
@@ -0,0 +1,18 @@
+
+//  ---------------space trim { aage or pichhe se trim hoti hai space}-------------------------------
+// -----SYNTAX----------
+
+// str.trim()
+
+// let msg = "  hello  ";
+//output of " hello  "  is "hello" butt 
+// if " he  llo " the output is "he  llo"
+
+
+let password = prompt("set your password");
+console.log(password.trim());
+
+//------------str.toLowerCase() or str.toUpperCase-------//
+  
+
+
